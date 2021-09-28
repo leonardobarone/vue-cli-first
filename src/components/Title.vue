4e7493f521b14867efe6ba0fc506742775919918
leonardobarone <leonardobarone26@gmail.com>
2021-09-28 15:33:11 +0200

    components title creato

---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
